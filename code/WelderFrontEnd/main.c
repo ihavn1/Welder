f84996d9f4c5f2d75eff2fec7d6c4c0aeb84f4aa
ihavn <ib@havns.dk>
2024-10-25 16:34:30 +0200

    Add AtmelStudio project

---
+++ code/WelderFrontEnd/main.c
@@ -0,0 +1,18 @@
+/*
+ * WelderFrontEnd.c
+ *
+ * Created: 25/10/2024 16.33.20
+ * Author : ib
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
